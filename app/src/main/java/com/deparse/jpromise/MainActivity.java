72ba71783f57aeff006711f72f66495400c92e01
huangshihai <365168907@qq.com>
2018-01-14 12:05:53 +0800

    sample

--- app/src/main/java/com/deparse/jpromise/MainActivity.java
+++ app/src/main/java/com/deparse/jpromise/MainActivity.java
@@ -3,11 +3,71 @@ package com.deparse.jpromise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.deparse.promise.JPromise;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JPromise(new JPromise.Fun() {
+
+            @Override
+            public void run(final JPromise promise, final Object... args) {
+                promise.resolve(0);
+            }
+        }).then(new JPromise.Fun() {
+            @Override
+            public void run(final JPromise promise, final Object... args) {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        int i = (Integer) args[0];
+                        System.out.println("i=" + i);
+                        promise.resolve(i + 1);
+                    }
+                }, 100);
+            }
+        }).then(new JPromise.Fun() {
+            @Override
+            public void run(final JPromise promise, final Object... args) {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        int i = (Integer) args[0];
+                        System.out.println("i=" + i);
+                        promise.resolve(i + 1);
+                    }
+                }, 100);
+            }
+        }).then(new JPromise.Fun() {
+            @Override
+            public void run(final JPromise promise, final Object... args) {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        int i = (Integer) args[0];
+                        System.out.println("i=" + i);
+                        promise.resolve(i + 1);
+                    }
+                }, 100);
+            }
+        }).then(new JPromise.Fun() {
+            @Override
+            public void run(final JPromise promise, final Object... args) {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        int i = (Integer) args[0];
+                        System.out.println("i=" + i);
+                    }
+                }, 100);
+            }
+        }).resolve();
     }
 }
